7335ae88aeadbb366596c23e78b541b803da0adf
Ju <94855929+PrincesLu@users.noreply.github.com>
2023-04-13 19:07:45 +0100

    Add files via upload

---
+++ Branches and Functions/ex35.py
@@ -0,0 +1,99 @@
+# sys is a library/ module short for system
+# we'll bring a special function
+from sys import exit 
+
+def gold_room():
+    # this function doesn't take any arguments
+    # but it needs to be called 
+    print("This room is full of gold. How much do you take? ")
+    # I added Enter a number
+
+    choice = input("> ")
+    # it takes whatever the person adds and assign it to the
+    # choice variable 
+
+    if "0" in choice or "1" in choice:
+        how_much = int(choice)
+    else:
+        dead("Man, learn to type a number. ")
+        # function "dead" is defined below
+
+
+    if how_much < 50:
+        print("Nice, you're not greedy, you win! ")
+        exit(0)
+        # exit is a system function that was imported at the top
+        # 0 is a code for an error-free exit (vs. 2, 16, 89, etc.)
+    else:
+        dead("You greedy bastard! Your're dead ")
+
+
+
+def bear_room():
+    print("There is a bear here. ")
+    print("The bear has a bunch of honey. ")
+    print("The fat bear is in front of another door. ")
+    print("How are you going to move the bear? ")
+    bear_moved = False
+    # bear_moved is a boolean variable
+
+    while True:
+        choice = input("> ")
+
+        if choice == "take honey":
+            dead("The bear looks at you then slaps your face off ")
+        elif choice == "taunt bear" and not bear_moved:
+            print("The bear has moved from the door. ")
+            print("You can go through it now. ")
+            bear_moved = True
+        elif choice == "taunt bear" and bear_moved:
+            dead("The bear gets pissed off and chews your leff off")
+        elif choice == "open door" and bear_moved:
+            gold_room()
+            # function "gold_room" was defined above
+        else:
+            print("I got no idea what that means")
+
+
+def cthulhu_room():
+    print("Here you see the great evil Cthulhu ")
+    print("She, he, whatever stares at you and you go insane")
+    print("Do you flee for your life or eat your head? ")
+
+    choice = input("> ")
+
+    if "flee" in choice:
+        # this looks at the text you entered anf i the keyword
+        # "flee" appears in it anywhere, it does this command
+        start()
+        # function "start" is defined below 
+    elif "head" in choice:
+        dead("Well that was tasty! ")
+    else:
+        cthulhu_room
+
+def dead(why):
+    print(why, "Good job! ")
+    exit(0)
+
+def start():
+    print("You are in a dark room. ")
+    print("There is a door to your right and left. ")
+    print("Which one do you take")
+
+    choice = input("> ")
+
+    if choice == "left":
+        bear_room()
+    elif choice == "right":
+        cthulhu_room()
+    else:
+        dead("You stumble around the room until you starve. ")
+
+start()
+# function "start" is defined above
+# this is the only function automaticaly run in this code 
+
+
+
+        
